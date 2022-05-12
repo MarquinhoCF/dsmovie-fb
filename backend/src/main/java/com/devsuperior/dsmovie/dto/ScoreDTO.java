c0332b5766a272023a070c22213441a31553c2e9
MarquinhoCF <marcos13cf@gmail.com>
2022-05-12 19:34:56 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
@@ -2,19 +2,19 @@ package com.devsuperior.dsmovie.dto;
 
 public class ScoreDTO {
 	
-	private Long moveId;
+	private Long movieId;
 	private String email;
 	private Double score;
 	
 	public ScoreDTO() {	
 	}
 
-	public Long getMoveId() {
-		return moveId;
+	public Long getMovieId() {
+		return movieId;
 	}
 
-	public void setMove_id(Long moveId) {
-		this.moveId = moveId;
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
 	}
 
 	public String getEmail() {
